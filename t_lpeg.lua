c43898ba05dd96451e5639b87f74b46e311d8fb8
Jinzheng Zhang <tianchaijz@gmail.com>
2016-11-01 17:20:48 +0800

    Add some test codes

--- t_lpeg.lua
+++ t_lpeg.lua
@@ -226,3 +226,50 @@ print("class", class:match"[abc]")
 
 local p = P"-" / ""
 print(p:match"-" == "")
+
+
+--------------------------------------------------------------------------------
+-- Inspect
+--------------------------------------------------------------------------------
+local function inspect(p)
+    print("==== ptree")
+    lpeg.ptree(p)
+    print("==== pcode")
+    lpeg.pcode(p)
+end
+
+local p = (P"-" * R"az" * R"AZ" * S"19")^3
+inspect(p)
+local p = (P"-" * R"AZ")^-3
+inspect(p)
+
+
+local p1 = Cc(true) * Cc("X")
+local p2 = p1 * Cg(Cc(true)) * Cc("Y") / print
+
+inspect(p2)
+inspect(Cc(true) * Cc("X") * Cg(Cc(true)) * Cc("Y") / print)
+
+
+--------------------------------------------------------------------------------
+-- C Comment Parser
+-- http://lua-users.org/wiki/LpegRecipes
+--------------------------------------------------------------------------------
+local BEGIN_COMMENT = lpeg.P("/*")
+local END_COMMENT = lpeg.P("*/")
+local NOT_BEGIN = (1 - BEGIN_COMMENT)^0
+local NOT_END = (1 - END_COMMENT)^0
+local FULL_COMMENT_CONTENTS = BEGIN_COMMENT * NOT_END * END_COMMENT
+
+-- Parser to find comments from a string
+local searchParser = (NOT_BEGIN * lpeg.C(FULL_COMMENT_CONTENTS))^0
+-- Parser to find non-comments from a string
+local filterParser = (lpeg.C(NOT_BEGIN) * FULL_COMMENT_CONTENTS)^0 * lpeg.C(NOT_BEGIN)
+
+-- Simpler version, although empirically it is slower.... (why?) ... any optimization
+-- suggestions are desired as well as optimum integration w/ C++ comments and other
+-- syntax elements
+local searchParser = (lpeg.C(FULL_COMMENT_CONTENTS) + 1)^0
+-- Suggestion by Roberto to make the search faster
+-- Works because it loops fast over all non-slashes, then it begins the slower match phase
+local searchParser = ((1 - lpeg.P"/")^0 * (lpeg.C(FULL_COMMENT_CONTENTS) + 1))^0
